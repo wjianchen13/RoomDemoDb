003fba7d5758baa72dbaacf6375a967c36ac0695
cwj <282330332@qq.com>
2025-08-21 14:17:15 +0800

    Room数据库的基础使用

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Room数据库
+
+# 编译环境
+android-studio-2024.3.2.15
+gradle插件：8.10.1
+gradle版本：gradle-8.11.1-bin
--- app/src/main/java/com/example/roomdemodb/MainActivity.kt
+++ app/src/main/java/com/example/roomdemodb/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.roomdemodb
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var user1: User
+    private lateinit var userDao: UserDao
+    private lateinit var user2: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,50 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        userDao = AppDatabase.getDatabase(this).userDao()
+        user1 = User("Tom", "Brady", 40)
+        user2 = User("Tom", "Hanks", 63)
     }
+
+    /**
+     * 插入数据
+     */
+    fun onTest1(v: View) {
+        thread {
+            user1.id = userDao.insertUser(user1)
+            user2.id = userDao.insertUser(user2)
+        }
+    }
+
+    /**
+     * 更新数据
+     */
+    fun onTest2(v: View) {
+        thread {
+            user1.age = 42
+            userDao.updateUser(user1)
+        }
+    }
+
+    /**
+     * 删除数据
+     */
+    fun onTest3(v: View) {
+        thread {
+            userDao.deleteUserByLastName("Hanks")
+        }
+
+    }
+
+    /**
+     * 加载全部数据
+     */
+    fun onTest4(v: View) {
+        thread {
+            for (user in userDao.loadAllUsers()) {
+                Log.d("MainActivity", user.toString())
+            }
+        }
+    }
+
 }
